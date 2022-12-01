5bbb21672f953befe6e80364538ec637aba89617
zerozelta <zerozelta@gmail.com>
2022-12-01 16:58:35 -0600

    fix: add key prop

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,9 +1,9 @@
 import {
-  faBookBookmark,
-  faLanguage,
-  faStar,
-  faTrophy,
-  faUser,
+    faBookBookmark,
+    faLanguage,
+    faStar,
+    faTrophy,
+    faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
@@ -14,124 +14,124 @@ import { SkillGroup } from "../src/lib/types/SkillData";
 import { fetchJSON } from "../src/lib/Utils";
 
 export default function Home() {
-  const [skills, setSkills] = useState<SkillGroup>();
+    const [skills, setSkills] = useState<SkillGroup>();
 
-  useEffect(() => {
-    fetchJSON("/static/data/skills.json").then((data) => {
-      console.log(data);
-      setSkills(data);
-    });
-  }, []);
+    useEffect(() => {
+        fetchJSON("/static/data/skills.json").then((data) => {
+            console.log(data);
+            setSkills(data);
+        });
+    }, []);
 
-  return (
-    <div className="flex w-full h-full flex-1 text-sm sm:text-lg">
-      <div className="flex flex-col gap-2 p-2">
-        <div className="sticky top-2 flex flex-col gap-2 text-blue-500">
-          <RoundedButton dark size={40}>
-            <FontAwesomeIcon icon={faUser} style={{ width: 22, height: 22 }} />
-          </RoundedButton>
-          <RoundedButton dark size={40}>
-            <FontAwesomeIcon icon={faStar} style={{ width: 22, height: 22 }} />
-          </RoundedButton>
-          <RoundedButton dark size={40}>
-            <FontAwesomeIcon
-              icon={faTrophy}
-              style={{ width: 22, height: 22 }}
-            />
-          </RoundedButton>
-          <RoundedButton dark size={40}>
-            <FontAwesomeIcon
-              icon={faBookBookmark}
-              style={{ width: 22, height: 22 }}
-            />
-          </RoundedButton>
-          <RoundedButton dark size={40}>
-            <FontAwesomeIcon
-              icon={faLanguage}
-              style={{ width: 22, height: 22 }}
-            />
-          </RoundedButton>
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center w-full h-full flex-1 m-auto max-w-[680px] gap-10 p-2">
-        <div className="flex flex-col justify-center items-center text-center gap-4">
-          <div>
-            Aldo César
-            <br />
-            Gutiérrez Gutiérrez
-          </div>
-
-          <div className="flex p-2 rounded-full overflow-hidden bg-white border-4 border-slate-100 w-fit">
-            <img
-              src="/static/img/photo.webp"
-              className="w-[125px] h-[125px] rounded-full"
-            />
-          </div>
-          <div className="flex flex-row items-center justify-center font-bold text-blue-500 gap-2">
-            <img src="/static/flags/mx.svg" height={25} width={25}></img>
-            <div>FullStack developer</div>
-          </div>
-
-          <div className="block w-full rounded-lg text-justify">
-            <strong>Software developer Engineer</strong> by the Polytechnic
-            University of Aguascalientes. Self-taught and programing
-            enthusiastic since I was 13 years old. with experience mainly in{" "}
-            <strong>software design </strong> and{" "}
-            <strong>web development technologies</strong>.
-          </div>
-
-          <div className="flex flex-row justify-start w-full gap-1">
-            <div className="rounded-md py-2 px-2 bg-slate-500 text-white text-xs w-fit whitespace-nowrap">
-              Self-taught
-            </div>
-            <div className="rounded-md py-2 px-2 bg-slate-500 text-white text-xs w-fit whitespace-nowrap">
-              Leadership
-            </div>
-            <div className="rounded-md py-2 px-2 bg-slate-500 text-white text-xs w-fit whitespace-nowrap">
-              Creative
-            </div>
-          </div>
-        </div>
-
-        <Section title="Skills & Technologies" icon={faStar}>
-          {skills &&
-            skills.map((group) => {
-              return (
-                <div>
-                  <div className="text-sm font-bold py-2">{group.title}</div>
-                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 w-full">
-                    {group.data &&
-                      group.data.map((skillItem) => {
-                        return <Skill item={skillItem} />;
-                      })}
-                  </div>
+    return (
+        <div className="flex w-full h-full flex-1 text-sm sm:text-lg">
+            <div className="flex flex-col gap-2 p-2">
+                <div className="sticky top-2 flex flex-col gap-2 text-blue-500">
+                    <RoundedButton dark size={40}>
+                        <FontAwesomeIcon icon={faUser} style={{ width: 22, height: 22 }} />
+                    </RoundedButton>
+                    <RoundedButton dark size={40}>
+                        <FontAwesomeIcon icon={faStar} style={{ width: 22, height: 22 }} />
+                    </RoundedButton>
+                    <RoundedButton dark size={40}>
+                        <FontAwesomeIcon
+                            icon={faTrophy}
+                            style={{ width: 22, height: 22 }}
+                        />
+                    </RoundedButton>
+                    <RoundedButton dark size={40}>
+                        <FontAwesomeIcon
+                            icon={faBookBookmark}
+                            style={{ width: 22, height: 22 }}
+                        />
+                    </RoundedButton>
+                    <RoundedButton dark size={40}>
+                        <FontAwesomeIcon
+                            icon={faLanguage}
+                            style={{ width: 22, height: 22 }}
+                        />
+                    </RoundedButton>
                 </div>
-              );
-            })}
-        </Section>
+            </div>
 
-        <Section title="Achievements" icon={faTrophy}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-          exercitationem cupiditate in itaque, at nihil quas earum, repudiandae
-          odio sit cumque quo impedit. Consequatur dolores voluptatem autem amet
-          repellat nobis.
-        </Section>
+            <div className="flex flex-col items-center w-full h-full flex-1 m-auto max-w-[680px] gap-10 p-2">
+                <div className="flex flex-col justify-center items-center text-center gap-4">
+                    <div>
+                        Aldo César
+                        <br />
+                        Gutiérrez Gutiérrez
+                    </div>
 
-        <Section title="Work experience" icon={faBookBookmark}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-          exercitationem cupiditate in itaque, at nihil quas earum, repudiandae
-          odio sit cumque quo impedit. Consequatur dolores voluptatem autem amet
-          repellat nobis.
-        </Section>
+                    <div className="flex p-2 rounded-full overflow-hidden bg-white border-4 border-slate-100 w-fit">
+                        <img
+                            src="/static/img/photo.webp"
+                            className="w-[125px] h-[125px] rounded-full"
+                        />
+                    </div>
+                    <div className="flex flex-row items-center justify-center font-bold text-blue-500 gap-2">
+                        <img src="/static/flags/mx.svg" height={25} width={25}></img>
+                        <div>FullStack developer</div>
+                    </div>
 
-        <Section title="Languages" icon={faLanguage}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-          exercitationem cupiditate in itaque, at nihil quas earum, repudiandae
-          odio sit cumque quo impedit. Consequatur dolores voluptatem autem amet
-          repellat nobis.
-        </Section>
-      </div>
-    </div>
-  );
+                    <div className="block w-full rounded-lg text-justify">
+                        <strong>Software developer Engineer</strong> by the Polytechnic
+                        University of Aguascalientes. Self-taught and programing
+                        enthusiastic since I was 13 years old. with experience mainly in{" "}
+                        <strong>software design </strong> and{" "}
+                        <strong>web development technologies</strong>.
+                    </div>
+
+                    <div className="flex flex-row justify-start w-full gap-1">
+                        <div className="rounded-md py-2 px-2 bg-slate-500 text-white text-xs w-fit whitespace-nowrap">
+                            Self-taught
+                        </div>
+                        <div className="rounded-md py-2 px-2 bg-slate-500 text-white text-xs w-fit whitespace-nowrap">
+                            Leadership
+                        </div>
+                        <div className="rounded-md py-2 px-2 bg-slate-500 text-white text-xs w-fit whitespace-nowrap">
+                            Creative
+                        </div>
+                    </div>
+                </div>
+
+                <Section title="Skills & Technologies" icon={faStar}>
+                    {skills &&
+                        skills.map((group, index) => {
+                            return (
+                                <div key={index}>
+                                    <div className="text-sm font-bold py-2">{group.title}</div>
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 w-full">
+                                        {group.data &&
+                                            group.data.map((skillItem, index) => {
+                                                return <Skill key={index} item={skillItem} />;
+                                            })}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                </Section>
+
+                <Section title="Achievements" icon={faTrophy}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                    exercitationem cupiditate in itaque, at nihil quas earum, repudiandae
+                    odio sit cumque quo impedit. Consequatur dolores voluptatem autem amet
+                    repellat nobis.
+                </Section>
+
+                <Section title="Work experience" icon={faBookBookmark}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                    exercitationem cupiditate in itaque, at nihil quas earum, repudiandae
+                    odio sit cumque quo impedit. Consequatur dolores voluptatem autem amet
+                    repellat nobis.
+                </Section>
+
+                <Section title="Languages" icon={faLanguage}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
+                    exercitationem cupiditate in itaque, at nihil quas earum, repudiandae
+                    odio sit cumque quo impedit. Consequatur dolores voluptatem autem amet
+                    repellat nobis.
+                </Section>
+            </div>
+        </div>
+    );
 }
