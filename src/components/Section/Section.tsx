89b4e657a4c40dcd1ffc29e080356d024bcf045b
Aldo César <agutierrez@rsimail.com>
2023-06-30 14:19:49 -0600

    general update

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -10,7 +10,7 @@ type SectionPorps = {
 };
 const Section = ({ children, title, icon, id }: SectionPorps) => {
   return (
-    <div className="flex w-full flex-col gap-2" id={id}>
+    <div className="flex w-full flex-col gap-2 py-2" id={id}>
       <h1 className="flex gap-2 text-lg text-blue-500 items-center">
         {icon && (
           <FontAwesomeIcon icon={icon} style={{ width: 15, height: 15 }} />
